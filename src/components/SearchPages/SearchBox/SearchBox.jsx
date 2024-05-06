cc8889c2bc123cdd420f6471526048265851097e
GabrielaEvtimova <147919001+GabrielaEvtimova@users.noreply.github.com>
2024-05-06 15:06:27 +0300

    Complete SearchBox component

--- src/components/SearchPages/SearchBox/SearchBox.jsx
+++ src/components/SearchPages/SearchBox/SearchBox.jsx
@@ -1,7 +1,46 @@
-import React from 'react'
+"use client";
+
+import React, { useState } from "react";
+import { AiOutlineSearch } from "react-icons/ai";
+import { BsFillMicFill } from "react-icons/bs";
+import { RxCross2 } from "react-icons/rx";
+import { useSearchParams, useRouter } from "next/navigation";
 
 export default function SearchBox() {
+  const searchParams = useSearchParams();
+  const term = searchParams.get("searchTerm");
+
+  const router = useRouter();
+
+  const [searchTerm, setSearchTerm] = useState(term || "");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!searchTerm.trim()) return;
+    router.push(`/search/web/?searchTerm=${searchTerm}`);
+  };
+
   return (
-    <div>SearchBox</div>
-  )
+    <form
+      className="flex border border-neutral-300 rounded-full shadow-lg px-6 py-3 ml-10 mr-5 flex-grow max-w-3xl items-center justify-between"
+      onSubmit={handleSubmit}
+    >
+      <input
+        type="text"
+        className="w-full focus:outline-none"
+        onChange={(e) => setSearchTerm(e.target.value)}
+        value={searchTerm}
+      />
+
+      <RxCross2
+        className="text-2xl text-neutral-500 cursor-pointer sm:mr-2"
+        onClick={() => setSearchTerm("")}
+      />
+      <BsFillMicFill className="pl-4 hidden sm:inline-flex text-4xl text-blue-500 border-l-2 border-neutral-300 mr-3" />
+      <AiOutlineSearch
+        className="hidden sm:inline-flex text-2xl text-blue-500 cursor-pointer"
+        onClick={handleSubmit}
+      />
+    </form>
+  );
 }

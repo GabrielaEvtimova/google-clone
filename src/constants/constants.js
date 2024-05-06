83fd78687d490f194a419b4eb49095137a7f4698
GabrielaEvtimova <147919001+GabrielaEvtimova@users.noreply.github.com>
2024-05-06 12:35:46 +0300

    Add constants.js

---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+export const GOOGLE_LOGO = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png";
+
+export const LOGO_ALT_TEXT = "Google Logo"

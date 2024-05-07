4c63805de421e9800ca37d0619104bbfa51c97a9
GabrielaEvtimova <147919001+GabrielaEvtimova@users.noreply.github.com>
2024-05-07 20:55:13 +0300

    Fix style in footer

--- src/components/FooterSection/Footer/Footer.jsx
+++ src/components/FooterSection/Footer/Footer.jsx
@@ -3,11 +3,11 @@ import CountryLookUp from "../CountryLookUp/CountryLookUp";
 
 export default function Footer() {
   return (
-    <footer className="absolute bottom-0 text-sm text-neutral-500 bg-[#f2f2f2] w-full">
+    <footer className="absolute bottom-0 text-[8pt] sm:text-sm text-neutral-500 bg-[#f2f2f2] w-full">
       <div className="border-b px-8 py-3">
         <CountryLookUp />
       </div>
-      <div className="flex flex-col sm:flex-row justify-between items-center px-8 py-3 space-y-7 sm:space-y-0">
+      <div className="flex flex-col sm:flex-row justify-between items-center px-8 py-3 space-y-1 sm:space-y-0">
         <ul className="flex items-center space-x-6">
           <li className="hover:underline cursor-pointer">About</li>
           <li className="hover:underline cursor-pointer">Advertising</li>
